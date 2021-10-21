82734ecd58783ba0ec16319a0546eee12ab4a47c
jasko255 <jasko255@gmail.com>
2021-10-21 19:03:23 +0200

    sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,51 @@
 import React from 'react'
 import './sidebar.css'
 
+import { RssFeed,  ChatSharp,  PlayCircle, Group, Bookmark, QuestionAnswerRounded, QuestionAnswer, HelpOutline, WorkOutline, Event, School } from '@mui/icons-material'
+
 function Sidebar() {
     return (
         <div className='sidebar'>
-            sidebar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+            <RssFeed className='sidebarIcon' />
+            <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <ChatSharp className='sidebarIcon' />
+            <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <PlayCircle className='sidebarIcon' />
+            <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <Group className='sidebarIcon' />
+            <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <Bookmark className='sidebarIcon' />
+            <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <HelpOutline className='sidebarIcon' />
+            <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <WorkOutline className='sidebarIcon' />
+            <span className="sidebarListItemText">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <Event className='sidebarIcon' />
+            <span className="sidebarListItemText">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+            <School className='sidebarIcon' />
+            <span className="sidebarListItemText">Courses</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
