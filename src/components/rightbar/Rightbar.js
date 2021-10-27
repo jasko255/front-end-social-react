e077d49f7f5340398e7d83cb6a474b4f92df006b
jasko255 <jasko255@gmail.com>
2021-10-27 12:52:19 +0200

    ok

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import './rightbar.css'
+import React from "react";
+import "./rightbar.css";
 
 function Rightbar() {
-    return (
-        <div className='rightbar'>
-            rightbar
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            {" "}
+            <b> Pola Foster </b> and 3 other friends have birthday today
+          </span>
         </div>
-    )
+        <img src="/assets/ad.png" className="rightbarAd" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Rightbar
+export default Rightbar;
